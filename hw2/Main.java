250f270c08b8c3b6e9eecfa7553fd5c4c42f6356
Zhukov Nikita <nypno@mail.ru>
2023-06-09 10:53:21 +0300

    added method to get the number float

--- hw2/Main.java
+++ hw2/Main.java
@@ -1,2 +1,28 @@
-package exception.homeWork.hw2;public class Main {
+package exception.homeWork.hw2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getFloatNum());
+    }
+
+    /*
+    Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+     и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
+      вместо этого, необходимо повторно запросить у пользователя ввод данных.
+     */
+
+    public static float getFloatNum() {
+        String num;
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите число: ");
+            num = sc.nextLine();
+            if (num.matches("[0-9]+")) return Float.parseFloat(num);
+            if (num.contains(",")) num = num.replace(",", ".");
+        }
+        while (!num.matches("^[0-9+]+\\.[0-9+]+$"));
+        return Float.parseFloat(num);
+    }
 }
