434cb57104fa48fc2eb69c86b063c5d56c8e61ab
Zhukov Nikita <nypno@mail.ru>
2023-06-12 11:18:21 +0300

    added solution hw2

--- hw2/Exercize2.java
+++ hw2/Exercize2.java
@@ -3,9 +3,13 @@ package exception.homeWork.hw2;
 public class Exercize2 {
     public static void main(String[] args) {
         // 2. Если необходимо, исправьте данный код:
-        int[] intArray = new int[]{3,1,5,234,8,6,1,9,8};
-        int d = 0;
+        int[] intArray = new int[]{3,5,12,33,1};
+        int d = 1;
         int arraySize = 8;
+        if (arraySize == 0){
+            System.out.println("Пустой массив! Необходимо заполнить его...");
+            return;
+        }
         if (d == 0) System.out.println("Делить на 0 нельзя!");
         else if (arraySize < intArray.length) {
             double catchedRes1 = intArray[arraySize] / d;
