0476f1373547c0c2636da3451f4124a72f5399b9
Zhukov Nikita <nypno@mail.ru>
2023-06-12 11:11:04 +0300

    added solution hw2

--- hw2/Exercize2.java
+++ hw2/Exercize2.java
@@ -5,9 +5,14 @@ public class Exercize2 {
         // 2. Если необходимо, исправьте данный код:
         int[] intArray = new int[]{3,1,5,234,8,6,1,9,8};
         int d = 0;
+        int arraySize = 8;
         if (d == 0) System.out.println("Делить на 0 нельзя!");
-        else {
-            double catchedRes1 = intArray[8] / d;
+        else if (arraySize < intArray.length) {
+            double catchedRes1 = intArray[arraySize] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } else {
+            arraySize = intArray.length - 1;
+            double catchedRes1 = intArray[arraySize] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
         }
 
